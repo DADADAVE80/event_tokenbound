8aa189b25b77c617f44f080580dc6975a8b01526
sogobanwo <sogobanwo@gmail.com>
2024-07-22 22:56:00 +0100

    feat: project set-up

---
+++ client/src/pages/dashboard/analytics.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Components/dashboard/layout'
+
+const Analytics = () => {
+    return (
+        <Layout>
+            <div>Analytics</div>
+        </Layout>
+    )
+}
+
+export default Analytics
